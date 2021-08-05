c8a3991b692d191ffd6306341bf2d377121ae902
Kaja Radošević <73023084+KajaRado7@users.noreply.github.com>
2021-08-05 11:12:48 +0200

    TopNav korekcije + Home.vue sadržaj/upute za app

--- src/App.vue
+++ src/App.vue
@@ -122,38 +122,7 @@
                   </li>
                 </ul>
               </li>
-              <li class="nav-item dropdown rightIcons">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarScrollDropdownNotif"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <!--NotificationIcon------>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="40"
-                    height="40"
-                    fill="currentColor"
-                    class="bi bi-bell"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"
-                    />
-                  </svg>
-                  <!--NotificationIcon------>
-                </a>
-                <ul
-                  class="dropdown-menu"
-                  aria-labelledby="navbarScrollingDropdown"
-                >
-                  <li><a class="dropdown-item" href="#">Notification1</a></li>
-                  <li><a class="dropdown-item" href="#">Notification2</a></li>
-                </ul>
-              </li>
+
               <li class="nav-item rightIcons">
                 <router-link to="/my_downloads">
                   <a class="nav-link" href="#">
@@ -505,7 +474,6 @@ export default {
 
 .bi-person-circle,
 .bi-download,
-.bi-bell,
 .bi-filter-square {
   color: white;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,32 @@
   <div class="row">
     <div class="col-2"></div>
     <div class="col-8 mainContent" style="text-align:justify;">
-      <p>
-        Click the
-        <b style="color:#16c937">green-plus-icon</b>
-        above to add a new Script!
-      </p>
+      <h3>
+        Welcome to
+        <b style="color:#8763b5">Scripto</b>
+        ! This app allows students to collaborate in terms of sharing scripts.
+        If you want to publish your script or are just searching for one,
+        everything is at your fingertips!
+        <br />
+        <br />
+        In terms of finding the required script, you have many options:
+        categorization by field, study and university, search by name or
+        creator, and even ranking depending on the number of downloads or the
+        highest grade.
+        <br />
+        <br />
+        If you are the creator of the best script ever, simply click on the
+        <b style="color:#16c937">green plus</b>
+        button in the upper left corner of the page and……Voila!
+      </h3>
     </div>
     <div class="col-2"></div>
   </div>
 </template>
 <style scoped>
+h3 {
+  text-align: justify;
+}
 .mainContent {
   margin-top: 250px;
   margin-left: 100px;
